a5f5b36e49a77bca41d9d248fd72c4db541e099d
Bhargav Domadiya <bhargavdomadiya00@gmail.com>
2023-08-17 14:52:43 +0530

    Initial Commit

---
+++ SocialAp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialAp
+//
+//  Created by R87 on 17/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
