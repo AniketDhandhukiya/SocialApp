02e6274d44fdc1befab6e810bfe9e4d472e8b047
SnehKalariya <snehkalariya9583@gmail.com>
2023-08-28 11:41:45 +0530

    28 Aug - Setting Page Complete

---
+++ SocialAp/SettingPageCell.swift
@@ -0,0 +1,14 @@
+//
+//  SettingPageCell.swift
+//  SocialAp
+//
+//  Created by Sneh Kalariya on 28/08/23.
+//
+
+import UIKit
+
+class SettingPageCell: UICollectionViewCell {
+    @IBOutlet weak var iconImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+
+}
