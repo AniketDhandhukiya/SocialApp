7db36fd1a14872624a34ffc32dbf833581500cbe
ikrrish <kishangovindiya1@gmail.com>
2023-08-28 15:37:09 +0530

    k

---
+++ SocialAp/CollectionViewCellForProfileStory.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCellForProfileStory.swift
+//  SocialAp
+//
+//  Created by R82 on 28/08/23.
+//
+
+import UIKit
+
+class CollectionViewCellForProfileStory: UICollectionViewCell {
+    
+    @IBOutlet weak var highlightImage: UIImageView!
+}
