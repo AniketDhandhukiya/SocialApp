4ef9b83deb9c7b5524b0fa3b77d8c12eed853855
SnehKalariya <snehkalariya9583@gmail.com>
2023-08-28 10:48:11 +0530

    28 Aug

--- SocialAp/SettingPage.swift
+++ SocialAp/SettingPage.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class SettingPage: UIViewController {
-
+    
+    var imageName = [UIImage (named: "Follow"), UIImage (named: "Notifications"), UIImage (named:
+    "Creator"), UIImage (named: "Privacy"), UIImage (named: "Payments"), UIImage (named:
+    "Security"), UIImage (named: "Ads"), UIImage (named: "user") , UIImage (named:
+    "Help"), UIImage (named: "About"), UIImage (named: "Theme" )]
+    
+    var labelName = ["Follow and invitefriends", "Notifications" , "Creator", "Privacy", "Payments", "Security", "Ads", "Accounts" , "Help", "About", "Theme"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension SettingPage : UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageName.count
     }
     
-
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! SettingPageCell
+        cell.iconImage.image = imageName[indexPath.row]
+        cell.nameLabel.text = labelName[indexPath.row]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout:UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 389, height: 60)
+    }
+    
 }
