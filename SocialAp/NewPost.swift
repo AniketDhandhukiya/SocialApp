3adaad086162cfd2c0bbc936846d751105419c1e
Bhargav Domadiya <bhargavdomadiya00@gmail.com>
2023-08-28 10:47:15 +0530

    aniket

--- SocialAp/NewPost.swift
+++ SocialAp/NewPost.swift
@@ -1,29 +1,39 @@
-//
-//  NewPost.swift
-//  SocialAp
-//
-//  Created by R87 on 17/08/23.
-//
+
 
 import UIKit
+import FirebaseCore
+import FirebaseAuth
+import FirebaseDatabase
+import FirebaseFirestore
 
-class NewPost: UIViewController {
+class NewPost: UIViewController, UINavigationControllerDelegate & UIImagePickerControllerDelegate {
 
+    @IBOutlet weak var ImageForUpload: UIImageView!
+    
+    var ref: DatabaseReference!
+    var refr: Firestore!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        ref = Database.database().reference()
+        refr = Firestore.firestore()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func gallaryButtonAction(_ sender: Any) {
+        openGallery()
     }
-    */
-
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        ImageForUpload.image = info[.editedImage] as! UIImage
+        dismiss(animated: true,completion: nil)
+    }
+    func openGallery(){
+        let gallery = UIImagePickerController()
+        gallery.delegate = self
+        gallery.allowsEditing = true
+        gallery.sourceType = .photoLibrary
+        present(gallery, animated: true,completion: nil)
+    }
+   
 }
